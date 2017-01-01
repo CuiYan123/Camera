4bc0e2baea889b82773ba3c7d24c90c9523f2d8a
崔艳 <cuiyan@cuiyandeMacBook-Pro.local>
2017-01-01 19:13:04 +0800

    Frist

---
+++ app/src/main/java/com/cuiyan/camera/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cuiyan.camera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
